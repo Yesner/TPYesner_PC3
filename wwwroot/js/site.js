91faf5636bd852e4f73d9173cf690463df544887
yesner <yesnersalgado@gmail.com>
2024-04-13 11:41:16 -0600

    Page Updated

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,24 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+document.querySelector('#createForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    var xhr = new XMLHttpRequest();
+    xhr.open(this.method, this.action, true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onload = function() {
+        if (xhr.status >= 200 && xhr.status < 400) {
+            document.getElementById('successModal').style.display = 'block';
+        }
+    };
+    xhr.send(new URLSearchParams(new FormData(this)).toString());
+});
+
+
+document.getElementById('closeModal').addEventListener('click', function() {
+    document.getElementById('successModal').style.display = 'none';
+    window.location.href = '/Home/Index'; // Asegúrate de que esta ruta sea correcta
+});
